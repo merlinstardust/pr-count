0f0532546a195daac1339f559780805fa0a9344c
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2024-03-01 02:14:30 -0500

    Add front UI and catch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,59 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from "react";
+import { getTotalPullRequests, PullRequestState } from "@/lib/GitHubApi";
 import styles from "./page.module.css";
 
+const pullRequestStates = ['all', 'open', 'closed'];
+
+function capitalize(string: string) {
+  return `${string[0].toUpperCase()}${string.slice(1)}`
+}
+
 export default function Home() {
+  const [ownerAndRepo, setOwnerAndRepo] = useState('lodash/lodash')
+  const [pullRequestState, setPullRequestState] = useState<PullRequestState>('all');
+  const [totalPullRequests, setTotalPullRequests] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (ownerAndRepo) {
+        const [owner, repo] = ownerAndRepo.split('/');
+        getTotalPullRequests({owner, repo, state: pullRequestState})
+          .then((total) => setTotalPullRequests(total))
+      }
+    }, 1000)
+    return () => clearTimeout(timer);
+  }, [ownerAndRepo, pullRequestState])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <div className="row">
+          <input 
+            type="text" 
+            value={ownerAndRepo} 
+            onChange={(event) => setOwnerAndRepo(event.target.value)}
+          />
+        </div>
+        <div className="row">
+          {pullRequestStates.map(prState =>
+            <button 
+              key={prState} 
+              className={pullRequestState === prState ? styles.active : styles.inactive}
+              disabled={pullRequestState === prState}
+              onClick={() => setPullRequestState(prState as PullRequestState)}
+            >
+              {capitalize(prState)}
+            </button>
+          )}
+        </div>
+        <div className="row">
+          <p className={styles.total}>
+            Total Pull Requests:
+            <br />
+            {totalPullRequests}
           </p>
-        </a>
+        </div>
       </div>
     </main>
   );
--- src/lib/GitHubApi.ts
+++ src/lib/GitHubApi.ts
@@ -11,38 +11,45 @@ type GitHubApiRequiredProps = {
   repo: string,
 }
 
+export type PullRequestState = 'all' | 'closed' | 'open'
+
 export type TotalPullRequestsProps = GitHubApiRequiredProps & {
-  state?: 'all' | 'closed' | 'open'
+  state?: PullRequestState
 }
 
 export async function getTotalPullRequests({owner, repo, state = 'all'}: TotalPullRequestsProps) {
-  const pullRequestsResponse = await octokit.rest.pulls.list({
-    owner, 
-    repo,
-    state,
-    per_page: 1,
-  });
+  try {
+    const pullRequestsResponse = await octokit.rest.pulls.list({
+      owner, 
+      repo,
+      state,
+      per_page: 1,
+    });
 
-  // If there's a link in headers, the first page will have a next link and a last link.
-  // Since each page only has one result, the last page number is the number of PRs.
-  // rel="next" and rel="last" should be in order
-  if (pullRequestsResponse.headers.link) {
-    const [, last] = pullRequestsResponse.headers.link.split(',');
-    const [bracketedUrl] = last.trim().split(';');
-    const urlString = bracketedUrl.replace(/[<>]/g, '');
-    const url = new URL(urlString);
-    const {page} = getSearchParamsAsObject(url.searchParams)
+    // If there's a link in headers, the first page will have a next link and a last link.
+    // Since each page only has one result, the last page number is the number of PRs.
+    // rel="next" and rel="last" should be in order
+    if (pullRequestsResponse.headers.link) {
+      const [, last] = pullRequestsResponse.headers.link.split(',');
+      const [bracketedUrl] = last.trim().split(';');
+      const urlString = bracketedUrl.replace(/[<>]/g, '');
+      const url = new URL(urlString);
+      const {page} = getSearchParamsAsObject(url.searchParams)
 
-    return Number(page);
+      return Number(page);
+    }
+
+    // If there's no link and either no data or data has length of 0,
+    // Then there are no PRs on this repository.
+    if (!pullRequestsResponse.data || pullRequestsResponse.data.length === 0) {
+      return 0;
+    }
+
+    // If there's no link, but there is data, its length must be 1
+    // Thus there is 1 PR on the repository
+    return 1;
   }
-
-  // If there's no link and either no data or data has length of 0,
-  // Then there are no PRs on this repository.
-  if (!pullRequestsResponse.data || pullRequestsResponse.data.length === 0) {
+  catch {
     return 0;
   }
-
-  // If there's no link, but there is data, its length must be 1
-  // Thus there is 1 PR on the repository
-  return 1;
 };
